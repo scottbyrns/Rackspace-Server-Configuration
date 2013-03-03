05b55a8f8861504bec763da1b9478367f630527e
Scott Byrns <contact@scottbyrns.com>
2013-03-03 08:15:30 -0700

    Adding additional tomcat parts.

--- install-tomcat6.sh
+++ install-tomcat6.sh
@@ -1,4 +1,4 @@
-apt-get install tomcat6 tomcat6-admin
+apt-get install tomcat6 tomcat6-common tomcat6-docs tomcat6-admin tomcat6-user
 
 # Portforward 80 to 8080
 iptables -A PREROUTING -t nat -i eth0 -p tcp --dport 80 -j REDIRECT --to-port 8080
